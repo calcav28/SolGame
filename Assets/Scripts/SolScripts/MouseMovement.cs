f65095c9279c15781f82835a4aaea6bb5a991546
calcav28 <cavanagh.calvin@gmail.com>
2023-01-11 23:09:51 -0800

    Barely Working Right Click Movement

--- Assets/Scripts/SolScripts/MouseMovement.cs
+++ Assets/Scripts/SolScripts/MouseMovement.cs
@@ -9,6 +9,7 @@ public class MouseMovement : MonoBehaviour
     public Camera cam;
     public Vector3 mousePos;
     public Vector3 movePosition;
+    public GameObject placeToMove;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +19,14 @@ public class MouseMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.rotation = new Quaternion(0, 0, 0, 0);
         if (Input.GetMouseButtonDown(1))
         {
             Vector3 mousePos = Input.mousePosition;
-            Vector3 movePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+            //Vector3 movePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+            Vector3 movePosition = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+            placeToMove.transform.position = movePosition;
         }
-        transform.position = Vector3.MoveTowards(transform.position, movePosition, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, placeToMove.transform.position, moveSpeed * Time.deltaTime);
     }
 }
