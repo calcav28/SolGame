b972ad4d638426753aeb8f57d3d01d613926d3b0
calcav28 <cavanagh.calvin@gmail.com>
2023-03-06 19:49:05 -0800

    See if this is even required

--- Assets/Scripts/SkillScripts/ShootSkillShot.cs
+++ Assets/Scripts/SkillScripts/ShootSkillShot.cs
@@ -6,49 +6,50 @@ public class ShootSkillShot : MonoBehaviour
 {
     public bool offCooldown = true;
     public GameObject shotPrefab;
+    public Transform firePoint;
     public float shotSpeed;
+    public Vector2 previousMovePosition;
+    public float fireForce = 20f;
     void Awake()
     {
-        shotSpeed = 7.0f;
+        shotSpeed = 20.0f;
+        fireForce = 20f;
     }
 
     void Update()
     {
-        //Vector2 shotDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 shotDirection = Input.mousePosition;
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 shootPosition = new Vector2(mousePos.x, mousePos.y);
-        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x)
-            * Mathf.Rad2Deg - 90f;
-        //transform.localRotation = Quaternion.Euler(0, 0, angle);
-
 
         if ((Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Q)) && offCooldown)
         {
-            GameObject.Find("Character").GetComponent<BasicMovement>().MoveSpeed = 0;
-            GameObject.Find("Character").GetComponent<MouseMovement>().moveSpeed = 0;
             Invoke("resetMoveSpeed", 0.5f);
             Invoke("useSkillShot", 0.25f);
         }
+
     }
 
     void resetMoveSpeed()
     {
-        GameObject.Find("Character").GetComponent<BasicMovement>().MoveSpeed = 5;
-        GameObject.Find("Character").GetComponent<MouseMovement>().moveSpeed = 5;
+        GameObject.Find("Character").GetComponent<MouseMovement>().canMove = true;
+        GameObject.Find("Character").GetComponent<MouseMovement>().placeToMove.transform.position = previousMovePosition;
     }
 
     //still doesn't work properly
     void useSkillShot()
     {
+        //stops player from moving after shooting
+        GameObject.Find("Character").GetComponent<MouseMovement>().canMove = false;
+        previousMovePosition = GameObject.Find("Character").GetComponent<MouseMovement>().movePosition;
+        
         Vector2 shotDirection = Input.mousePosition;
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(shotDirection);
         Vector2 shootPosition = new Vector2(mousePos.x, mousePos.y);
+        shootPosition.Normalize();
         float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x)
             * Mathf.Rad2Deg - 90f;
-        GameObject shot = Instantiate(shotPrefab, transform.position, Quaternion.Euler(0, 0, angle));
-        shot.GetComponent<Rigidbody2D>().velocity = shootPosition.normalized * shotSpeed;
+        
+        GameObject.Find("Character").GetComponent<MouseMovement>().placeToMove.transform.position = this.transform.position;
+        
+        GameObject shot = Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
+        shot.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
     }
-
-
 }
