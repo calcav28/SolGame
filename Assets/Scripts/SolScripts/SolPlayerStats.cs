ac9693fdd7e422e66ee800389911b1b596b440dc
calcav28 <cavanagh.calvin@gmail.com>
2023-07-04 17:37:38 -0700

    Small Changes

--- Assets/Scripts/SolScripts/SolPlayerStats.cs
+++ Assets/Scripts/SolScripts/SolPlayerStats.cs
@@ -11,7 +11,7 @@ public class SolPlayerStats : MonoBehaviour
 
 
     //keybinds and preferences
-    public bool wasdMovement;
+    public bool wasdMovement; //can be turned into int for more than 2 movement systems
     //need keybinds for q, e, r, f, and spacebar abilities
 
     //currency for abilties
