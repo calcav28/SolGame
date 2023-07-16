be323fccc6f14786fa8775c7d7508b34b1bfd5af
calcav28 <cavanagh.calvin@gmail.com>
2023-07-16 04:05:20 -0700

    welp

--- Assets/Scripts/SolScripts/RotateAroundPoint.cs
+++ Assets/Scripts/SolScripts/RotateAroundPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //This script handles the circles rotating around the player alongside abilities for player 
 
