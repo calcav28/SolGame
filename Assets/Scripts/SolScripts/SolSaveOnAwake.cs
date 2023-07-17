3cd98b884543e94244d33127c39822be6f5afe09
calcav28 <cavanagh.calvin@gmail.com>
2023-07-17 00:33:02 -0700

    Work on Setting and Saving High Scores Depending on Difficulty

--- Assets/Scripts/SolScripts/SolSaveOnAwake.cs
+++ Assets/Scripts/SolScripts/SolSaveOnAwake.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SolSaveOnAwake : MonoBehaviour
 {
+
 
     // Start is called before the first frame update
     void Awake()
@@ -15,5 +17,44 @@ public class SolSaveOnAwake : MonoBehaviour
         //adds points gained from played game to total superPoints
         GameObject.Find("GlobalScripts").GetComponent<GeneralPlayerStats>().superPoints += 
         GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all;
+
+
+
+        //handles setting high score depending on difficulty
+        switch (GameObject.Find("GlobalScripts").GetComponent<SolGameStats>().gameDifficulty)
+        {
+            case "easy":
+                if (GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solEasyHighScore <
+                    GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all)
+                {
+                    GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solEasyHighScore =
+                    GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all;
+                }
+                break;
+
+            case "medium":
+                if (GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solMedHighScore <
+                    GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all)
+                {
+                    GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solMedHighScore =
+                    GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all;
+                }
+                break;
+            case "hard":
+                if (GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solHardHighScore <
+                    GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all)
+                {
+                    GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solHardHighScore =
+                    GameObject.Find("HighScoreTest").GetComponent<GetHighScore>().all;
+                }
+                break;
+        }
+
+        GameObject.Find("EasyHighScoreNumber").GetComponent<TMP_Text>().text = GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solEasyHighScore.ToString();
+        GameObject.Find("MediumHighScoreNumber").GetComponent<TMP_Text>().text = GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solMedHighScore.ToString();
+        GameObject.Find("HardHighScoreNumber").GetComponent<TMP_Text>().text = GameObject.Find("GlobalScripts").GetComponent<SolPlayerStats>().solHardHighScore.ToString();
+
+        //saves everything
+        GameObject.Find("GlobalScripts").GetComponent<SaveLoadSystem>().saveToJson();
     }
 }
